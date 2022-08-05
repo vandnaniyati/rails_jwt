ad5a88dd3acc25b5510f2903810bc82f75cb14f2
vandna <vandna@niyati.com>
2022-08-05 13:58:19 +0530

    add product model

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,53 @@
+class ProductsController < ApplicationController
+  before_action :set_product, except: %i[ create index ]
+
+  # GET /products
+  def index
+    @products = Product.all
+
+    render json: @products
+  end
+
+  # GET /products/1
+  def show
+    render json: @product
+  end
+
+  # POST /products
+  def create
+    @product = Product.new(product_params)
+     
+    if @product.save
+      render json: @product, status: :created, location: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /products/1
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /products/1
+  def destroy
+    @product.destroy
+  end
+
+  private
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def product_params
+      params.require(:product).permit(:name, :price)
+      #params.permit(:name, :price)
+    end
+end
+
